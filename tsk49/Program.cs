29781876cfd16a728c55194f12816277057f1ef3
Artem <40538552+surge603@users.noreply.github.com>
2022-02-27 19:42:34 +0300

    Задача 49

---
+++ tsk49/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 49: Показать двумерный массив размером m×n, заполненный вещественными числами.
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int y = int.Parse(Console.ReadLine());
+double[,] arr = new double[x, y];
+
+FillArray();
+PrintArray();
+
+void FillArray()
+{
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            arr[i, j] = Math.Sqrt(new Random().Next(10, 100));
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+           Console.Write ($"{Math.Round(arr[i, j],2)} ");
+        }
+        Console.WriteLine();
+    }
+
+}
